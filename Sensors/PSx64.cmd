3c0790deec88fba6f492bfcc33fb7dece50c48e1
Andi Bellstedt <andi@bellstedt.anonymous>
2022-11-25 23:18:56 +0100

    64Bit execution script

---
+++ Sensors/PSx64.cmd
@@ -0,0 +1,10 @@
+@echo off
+setlocal enabledelayedexpansion
+
+SET AllParameter=%*
+SET FirstParameter=%1
+SET replace=
+CALL SET scriptParameter=%%AllParameter:!FirstParameter!=!replace!%%
+
+"%SystemRoot%\sysnative\WindowsPowerShell\v1.0\powershell.exe" -ExecutionPolicy RemoteSigned -NoLogo -NonInteractive -NoProfile -File "C:\Program Files (x86)\PRTG Network Monitor\Custom Sensors\EXEXML\%FirstParameter%"%scriptParameter%
+rem "%SystemRoot%\sysnative\WindowsPowerShell\v1.0\powershell.exe" -ExecutionPolicy RemoteSigned -NoLogo -NonInteractive -NoProfile -Command "& { .'C:\Program Files (x86)\PRTG Network Monitor\Custom Sensors\EXEXML\%FirstParameter%'%scriptParameter% }"
